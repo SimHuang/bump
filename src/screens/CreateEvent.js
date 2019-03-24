4d61a779399d7a1bd771e6bdd82f25149eb8d2d8
Terence Lau <tlau898@gmail.com>
2019-03-24 05:04:03 -0400

    +Cleaned up create event screen, added validation of null entries

--- src/screens/CreateEvent.js
+++ src/screens/CreateEvent.js
@@ -4,9 +4,10 @@ import firebase from '@firebase/app';
 import '@firebase/auth';
 import '@firebase/database';
 
-import { Alert, TouchableOpacity, View, Text, Picker } from 'react-native';
-import { Header, Icon, Input, Button } from 'react-native-elements';
+import { Alert, TouchableOpacity, View, Text } from 'react-native';
+import { Header, Icon, Input, Divider, Button } from 'react-native-elements';
 import DateTimePicker from 'react-native-modal-datetime-picker';
+import { Container, Content, Picker, Form, Item, List, ListItem } from "native-base";
 
 class CreateEvent extends React.Component {
   constructor(props) {
@@ -14,9 +15,9 @@ class CreateEvent extends React.Component {
     this.state = {
       eventTitle: '',
       eventDescription: '',
-      eventCategory: 'Sports',
+      eventCategory: '',
       eventDate: '',
-      eventAvailableSpots: 1,
+      eventAvailableSpots: '',
       message: '',
       isDateTimePickerVisible: false
     }
@@ -52,6 +53,17 @@ class CreateEvent extends React.Component {
   };
 
   createEvent() {
+
+    if(!this.state.eventTitle || 
+       !this.state.eventDescription || 
+       !this.state.eventCategory || 
+       !this.state.eventDate || 
+       !this.state.eventAvailableSpots)
+    {
+      Alert.alert("Please fill in all fields");
+      return;
+    }
+
     const newPostKey = firebase.database().ref().child('posts').push().key;
     const userId = firebase.auth().currentUser.uid;
     firebase.database().ref('/events/' + newPostKey).set({
@@ -84,7 +96,8 @@ class CreateEvent extends React.Component {
   render() {
     return (
       <View style={{backgroundColor: 'white', flex:1}}>
-        <Header
+        <Header 
+          containerStyle = {{backgroundColor: '#1e9e88' }}
           leftComponent={<Icon name="angle-left" 
                               type="font-awesome" 
                               onPress={() => this.props.navigation.navigate('AppNavigator')}/>}
@@ -110,27 +123,39 @@ class CreateEvent extends React.Component {
 
         <View style={{flexDirection: 'row', alignItems: 'center'}}>
           <View style={{flex:.5}}>
-            <Text>{'   Select Category:'}</Text>
+            <ListItem>
+              <Text>Select Category</Text>
+            </ListItem>
           </View>
-          <View style={{flex:.2}}>
-            <Picker
-              mode="dropdown"
-              selectedValue={this.state.eventCategory}
-              style={{height: 50, width: 500}}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({eventCategory: itemValue})
-              }>
-              <Picker.Item label="Sports" value="Sports" />
-              <Picker.Item label="Party" value="Party" />
-              <Picker.Item label="Food" value="Food" />
-            </Picker>
+          <View style={{flex:.5}}>
+          <Picker
+            iosIcon={<Icon name="arrow-down" />}
+            mode='dropdown'
+            selectedValue={this.state.eventCategory}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({eventCategory: itemValue})
+            }>
+            <Picker.Item it label="..." value='' />
+            <Picker.Item label="Sports" value="Sports" />
+            <Picker.Item label="Party" value="Party" />
+            <Picker.Item label="Food" value="Food" />
+          </Picker>
           </View>
         </View>
 
-        <View style={{ flex: 1 }}>
-          <Button
-            title="Select Time"
-            onPress={this.showDateTimePicker} />
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{flex:.5}}>
+          <ListItem>
+              <Text>Select DateTime</Text>
+            </ListItem>
+          </View>
+          <View style={{flex:.5}}>
+            <Button
+              type = "outline"
+              title={ !this.state.eventDate ? "..." : this.state.eventDate}
+              onPress={this.showDateTimePicker} />
+          </View>
+
           <DateTimePicker
             mode="datetime"
             isVisible={this.state.isDateTimePickerVisible}
@@ -139,9 +164,14 @@ class CreateEvent extends React.Component {
           />
         </View>
 
-        <Button
-          title="Create Event" 
-          onPress={() => this.createEvent()} />
+        <View style={{flex:1, justifyContent: 'flex-end', marginBottom:36}}>
+          <Button
+            title="Create Event" 
+            type = "outline"
+            color="orange"
+            onPress={() => this.createEvent()} />
+        </View>
+
         <Text>{this.state.message}</Text>
       </View>
     )
